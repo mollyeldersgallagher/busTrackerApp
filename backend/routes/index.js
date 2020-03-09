97ecca9c729fb7efc1a616e4e09fe7be3e9d4de1
Molly Elders Gallagher <44023005+mollyeldersgallagher@users.noreply.github.com>
2020-03-09 19:26:29 +0000

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,13 +5,6 @@ const rtpi = require("./rtpi");
 const authenticate = require("../middlewares/authenticate");
 
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.status(200).send({
-      message:
-        "Welcome to the AUTHENTICATION API. Register or Login to test Authentication."
-    });
-  });
-
   app.use("/api/auth", auth);
   app.use("/api/rtpi", rtpi);
   app.use("/api/user", authenticate, user);
