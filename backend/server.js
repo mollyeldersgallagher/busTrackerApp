fb5d0c6aeaef20df0e74df5705174e0241a8ee4f
Molly Elders Gallagher <44023005+mollyeldersgallagher@users.noreply.github.com>
2020-03-10 12:46:14 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const path = require("path");
 
 // Setting up port
 const uri = process.env.ATLAS_URI;
-let PORT = process.env.PORT || 8000;
+let PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//mongoose.promise = global.Promise;
+mongoose.promise = global.Promise;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -27,10 +27,10 @@ const connection = mongoose.connection;
 connection.once("open", () =>
   console.log("MongoDB Atlas database connected successfully!")
 );
-// connection.on("error", err => {
-//   console.log("MongoDB connection error" + err);
-//   process.exit();
-// });
+connection.on("error", err => {
+  console.log("MongoDB connection error" + err);
+  process.exit();
+});
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -45,4 +45,4 @@ require("./middlewares/jwt")(passport);
 require("./routes/index")(app);
 
 //server running
-app.listen(PORT, () => console.log("Server running on port" + PORT));
+app.listen(PORT, () => console.log("Server running on port " + PORT));
